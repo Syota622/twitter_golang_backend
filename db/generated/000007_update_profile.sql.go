90405e52392f22c71a585de299d161d0f9c31414
Shota <wvp360gqanukcqhg@gmail.com>
2024-03-05 23:44:00 +0900

    update_atの更新処理を追加

--- db/generated/000007_update_profile.sql.go
+++ db/generated/000007_update_profile.sql.go
@@ -12,7 +12,7 @@ import (
 
 const updateUserProfile = `-- name: UpdateUserProfile :exec
 UPDATE users
-SET username = $2, email = $3, bio = $4, profile_image_url = $5, background_image_url = $6
+SET username = $2, email = $3, bio = $4, profile_image_url = $5, background_image_url = $6, updated_at = CURRENT_TIMESTAMP
 WHERE id = $1
 `
 
