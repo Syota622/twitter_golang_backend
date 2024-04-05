c4c0d985098e262b7b4e8bec94cfc1e206edd45a
Shota <wvp360gqanukcqhg@gmail.com>
2024-04-05 15:39:09 +0900

    フォローの実装が完了

--- api/follow.go
+++ api/follow.go
@@ -39,3 +39,36 @@ func CreateFollowHandler(db *generated.Queries) gin.HandlerFunc {
 		c.JSON(http.StatusOK, gin.H{"follow": follow})
 	}
 }
+
+// IsFollowingHandler はログインユーザーが指定したユーザーをフォローしているかどうかを確認するためのハンドラ
+func IsFollowingHandler(db *generated.Queries) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// 認証されたユーザーのIDを取得
+		userID, exists := c.Get("userID")
+		if !exists {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "認証が必要です"})
+			return
+		}
+
+		// パラメーターから取得: フォロー状態を確認するユーザーのIDを取得
+		followIdParam, err := strconv.Atoi(c.Param("followId"))
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "無効なfollowIdパラメータ"})
+			return
+		}
+
+		// フォローの状態を取得
+		arg := generated.IsFollowingParams{
+			UserID:   int32(userID.(int)),  // ユーザーIDの取得
+			FollowID: int32(followIdParam), // フォローIDの取得
+		}
+		isFollowing, err := db.IsFollowing(c, arg)
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "フォローの状態の取得に失敗しました"})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"isFollowing": isFollowing})
+	}
+}
